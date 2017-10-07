3f2d59b7131749303e44e4923ee7f7b1298f406f
GBelchior <gabriel.acostab@hotmail.com>
2017-10-07 15:26:50 -0300

    Moved resources from CDN to local storage

--- Educon/App_Start/BundleConfig.cs
+++ Educon/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace Educon
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/material").Include(
+                        "~/Scripts/material.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,7 +28,12 @@ namespace Educon
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css", "~/Content/login.css", "~/Content/cadastro.css", "~/Content/portal.css"));
+                      "~/Content/site.css",
+                      "~/Content/login.css",
+                      "~/Content/cadastro.css",
+                      "~/Content/portal.css",
+                      "~/Content/material.min.css",
+                      "~/Content/material.icons.css"));
         }
     }
 }

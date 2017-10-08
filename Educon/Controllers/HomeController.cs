9b9be731a882bf5ef01d6772487f1576aec773bf
GBelchior <gabriel.acostab@hotmail.com>
2017-10-08 20:13:44 -0300

    201710082013

--- Educon/Controllers/HomeController.cs
+++ Educon/Controllers/HomeController.cs
@@ -9,26 +9,8 @@ namespace Educon.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        public ActionResult Portal()
-        {
-            return View();
-        }
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
